c329cd02182715ceb64335844fb0884e3d779205
Konoha-orz <517136675@qq.com>
2017-09-20 10:51:22 +0800

    SSM01 Build

---
+++ SSM01/src/main/java/com/demo/org/bean/Role.java
@@ -0,0 +1,51 @@
+package com.demo.org.bean;
+
+public class Role {
+
+	private int id;
+
+	private String role_name;
+
+	private String description;
+
+	public int getId() {
+
+		return id;
+
+	}
+
+	public void setId(int id) {
+
+		this.id = id;
+
+	}
+
+	public String getRolename() {
+
+		return role_name;
+
+	}
+
+	public void setRolename(String rolename) {
+
+		this.role_name = rolename;
+
+	}
+
+	public String getDescription() {
+
+		return description;
+
+	}
+
+	public void setDescription(String description) {
+
+		this.description = description;
+
+	}
+
+	
+
+	
+
+}
---
+++ SSM01/src/main/java/com/demo/org/controller/IndexController.java
@@ -0,0 +1,38 @@
+package com.demo.org.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.demo.org.bean.Account;
+import com.demo.org.mapper.IAccountService;
+import com.demo.org.service.IAccount;
+
+@Controller
+
+@RequestMapping("/index")
+
+public class IndexController {
+
+	@Resource
+	private IAccount accountService;
+	
+	@Resource
+	private IAccountService iac;
+	
+	@RequestMapping()
+
+	public String index(ModelMap resultMap) {
+		
+		List<Account> accountList = iac.getAll();//accountService.getCount();
+
+		resultMap.addAttribute("account",accountList.get(1));
+		return "index";
+
+	}
+
+}
